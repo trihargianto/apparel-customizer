c052fac4a3deaaa06ed109e2ac9e5d469c738f8a
Tri Hargianto <trihargianto@gmail.com>
2018-01-12 14:28:48 +0700

    memindah App ke containers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./src/App";
+import App from "./src/containers/App";
 import { Provider } from "react-redux";
 import store from "./src/store/configureStore";
 import "bootstrap/dist/css/bootstrap.min.css";
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Logo from "./components/Logo";
-import LeftSideBarMenu from "./components/LeftSideBarMenu";
+import Logo from "../components/Logo";
+import LeftSideBarMenu from "../components/LeftSideBarMenu";
 
 class App extends React.PureComponent {
   render() {
