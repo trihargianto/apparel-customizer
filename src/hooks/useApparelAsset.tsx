12b88581e36fd8674e15be1a7fc932c7c20f1b2e
Tri Hargianto <trihargianto@gmail.com>
2023-12-04 09:21:00 +0700

    define canvas area on apparel

--- src/hooks/useApparelAsset.tsx
+++ src/hooks/useApparelAsset.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import { useRef, useEffect } from "react";
+import * as fabric from "fabric";
 
 /**
  * TYPES
@@ -25,14 +26,57 @@ export const useApparelAsset = (
   options?: ApparelOptionTypes,
 ) => {
   const { color = "black", gender = "male", side = "front" } = options ?? {};
+  const canvasElement = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    // @ts-ignore-next
+    const canvas = new fabric.Canvas(canvasElement.current);
+
+    // TODO: Remove dummyRectangle
+    const dummyRectangle = new fabric.Rect({
+      backgroundColor: "black",
+      fill: "black",
+      width: 50,
+      height: 50,
+    });
+
+    canvas.add(dummyRectangle);
+
+    return () => {
+      canvas.dispose();
+    };
+
+    // TODO: Make canvas available to the app via context
+  }, [color]);
 
   const ImageComponent = () => (
-    <Image
-      src={`/apparels/${apparel}/${apparel}-${gender}-${color}-${side}.png`}
-      alt="Tshirt Male Front"
-      width={530}
-      height={630}
-    />
+    <div
+      style={{
+        backgroundImage: `url('/apparels/${apparel}/${apparel}-${gender}-${color}-${side}.png')`,
+        backgroundSize: "480px 559px",
+        backgroundRepeat: "no-repeat",
+        width: 530,
+        height: 630,
+      }}
+      className="relative"
+    >
+      <canvas
+        ref={canvasElement}
+        style={{
+          border: "1px dashed #f0f0f0",
+          position: "absolute",
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
+          margin: "auto",
+          marginTop: "110px",
+          marginLeft: "148px",
+        }}
+        width={180}
+        height={220}
+      ></canvas>
+    </div>
   );
 
   return { ImageComponent };
