4ad8aabf8f777367ced70d612d2781dd78dc0486
Tri Hargianto <trihargianto@gmail.com>
2023-12-10 21:57:42 +0700

    make apparel uses fabric canvas (#64)

--- src/components/02-molecules/ApparelCanvas/ApparelCanvas.tsx
+++ src/components/02-molecules/ApparelCanvas/ApparelCanvas.tsx
@@ -1,8 +1,10 @@
 import { useRef, useEffect, useContext } from "react";
+import type { FabricImage } from "fabric";
 import * as fabric from "fabric";
 
 import { ObjectDispatchContext } from "@/contexts/ObjectContext";
 import { CanvasDispatchContext } from "@/contexts/CanvasContext";
+import { useScreenDetect } from "@/hooks/useScreenDetect";
 
 /**
  * TYPES
@@ -22,11 +24,36 @@ const ApparelCanvas = ({
 }: ApparelOptionTypes) => {
   const dispatchCanvas = useContext(CanvasDispatchContext);
   const dispatchObject = useContext(ObjectDispatchContext);
+
   const canvasElement = useRef<HTMLCanvasElement>(null);
+
+  const { isMediumScreen, isLargeScreen, isXLargeScreen } = useScreenDetect();
+
+  // Mobile-first!
+  let canvasWidth = 340;
+
+  if (isXLargeScreen) {
+    canvasWidth = 550;
+  } else if (isLargeScreen) {
+    canvasWidth = 370;
+  } else if (isMediumScreen) {
+    canvasWidth = 800;
+  }
 
   useEffect(() => {
     // @ts-ignore-next
-    const fabricCanvas = new fabric.Canvas(canvasElement.current);
+    const fabricCanvas = new fabric.Canvas(canvasElement.current, {
+      width: canvasWidth,
+      height: canvasWidth,
+    });
+
+    fabric.FabricImage.fromURL(
+      `/apparels/${apparel}/${apparel}-${gender}-${color}-${side}.png`,
+    ).then((imageObject: FabricImage) => {
+      imageObject.scaleToWidth(canvasWidth);
+      fabricCanvas.set("backgroundImage", imageObject);
+      fabricCanvas.renderAll();
+    });
 
     dispatchCanvas({
       type: "canvas-updated",
@@ -59,36 +86,38 @@ const ApparelCanvas = ({
     return () => {
       fabricCanvas.dispose();
     };
-  }, [dispatchCanvas, dispatchObject]);
+  }, [apparel, color, dispatchCanvas, dispatchObject, gender, side]);
 
-  return (
-    <div
-      style={{
-        backgroundImage: `url('/apparels/${apparel}/${apparel}-${gender}-${color}-${side}.png')`,
-        backgroundSize: "480px 559px",
-        backgroundRepeat: "no-repeat",
-        width: 530,
-        height: 630,
-      }}
-      className="relative"
-    >
-      <canvas
-        ref={canvasElement}
-        style={{
-          position: "absolute",
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          margin: "auto",
-          marginTop: "110px",
-          marginLeft: "148px",
-        }}
-        width={180}
-        height={220}
-      ></canvas>
-    </div>
-  );
+  return <canvas ref={canvasElement} />;
+
+  // return (
+  //   <div
+  //     style={{
+  //       backgroundImage: `url('/apparels/${apparel}/${apparel}-${gender}-${color}-${side}.png')`,
+  //       backgroundSize: "480px 559px",
+  //       backgroundRepeat: "no-repeat",
+  //       width: 530,
+  //       height: 630,
+  //     }}
+  //     className="relative"
+  //   >
+  //     <canvas
+  //       ref={canvasElement}
+  //       style={{
+  //         position: "absolute",
+  //         left: 0,
+  //         right: 0,
+  //         top: 0,
+  //         bottom: 0,
+  //         margin: "auto",
+  //         marginTop: "110px",
+  //         marginLeft: "148px",
+  //       }}
+  //       width={180}
+  //       height={220}
+  //     ></canvas>
+  //   </div>
+  // );
 };
 
 export default ApparelCanvas;
