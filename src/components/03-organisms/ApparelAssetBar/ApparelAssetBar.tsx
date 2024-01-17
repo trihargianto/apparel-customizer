aa3fb38b615f37b44ba4dd83b46da4824b4e870c
Tri Hargianto <trihargianto@gmail.com>
2024-01-17 22:22:57 +0700

    feat(app): #68 add upload image feature (#69)
    Closes: #68

--- src/components/03-organisms/ApparelAssetBar/ApparelAssetBar.tsx
+++ src/components/03-organisms/ApparelAssetBar/ApparelAssetBar.tsx
@@ -1,6 +1,6 @@
 import type { EmojiClickData } from "emoji-picker-react";
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import dynamic from "next/dynamic";
 import { Tab } from "@headlessui/react";
 import clsx from "clsx";
@@ -10,7 +10,6 @@ import { useFabric } from "@/hooks/useFabric";
 import Card from "@/components/01-atoms/Card";
 import Button from "@/components/01-atoms/Button";
 import Input from "@/components/01-atoms/Input";
-import ComingSoon from "@/components/02-molecules/ComingSoon";
 import ShapePicker, { ShapeTypes } from "@/components/02-molecules/ShapePicker";
 
 type ApparelAssetPropTypes = {
@@ -57,7 +56,23 @@ const ApparelAssetsBar = ({ className }: ApparelAssetPropTypes) => {
         break;
       default:
         throw new Error("`shape` is not defined");
-        break;
+    }
+  }
+
+  function handleInputFileChange(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const result = e?.target?.result;
+
+        if (result) {
+          // @ts-ignore-next
+          addImageToCanvas(result);
+        }
+      };
+
+      reader.readAsDataURL(e.target.files[0]);
     }
   }
 
@@ -127,7 +142,36 @@ const ApparelAssetsBar = ({ className }: ApparelAssetPropTypes) => {
           {/* Image Menu Content */}
           <Tab.Panel>
             <Card isBordered={false}>
-              <ComingSoon />
+              <div className="flex w-full items-center justify-center">
+                <label className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100">
+                  <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                    <svg
+                      className="mb-4 h-8 w-8 text-gray-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                      />
+                    </svg>
+                    <p className="mb-2 text-sm text-gray-500">
+                      <span className="font-semibold">Click to upload</span>
+                    </p>
+                    <p className="text-xs text-gray-500">PNG or JPG</p>
+                  </div>
+                  <input
+                    type="file"
+                    className="hidden"
+                    accept="image/png, image/jpeg"
+                    onChange={handleInputFileChange}
+                  />
+                </label>
+              </div>
             </Card>
           </Tab.Panel>
         </Tab.Panels>
